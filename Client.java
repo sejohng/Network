cb614077ac5e12fdd3040cf64fc0e5a3377a9fd7
Shijun Jiang <lanyechan@Sjohngs-MacBook-Pro.local>
2024-02-26 20:35:04 -0600

    Initial commit

---
+++ Client.java
@@ -0,0 +1,91 @@
+/*
+Shijun Jiang
+CSCI 4311 Socket Programming (Assignment 1)
+Spring 2024 
+Client
+*/
+
+import java.io.*;
+import java.net.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Client {
+    private static volatile boolean running = true;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: java Client <host name> <port number>"); //java Client localhost 8989
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        long startTime = System.currentTimeMillis();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            long endTime = System.currentTimeMillis();
+            long totalTimeMillis = endTime - startTime;
+            String totalTime = String.format("%d:%02d.%04d",
+                                            totalTimeMillis / 60000,
+                                            (totalTimeMillis / 1000) % 60,
+                                            totalTimeMillis % 1000);
+
+            String finishedAt = ZonedDateTime.now().format(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy"));
+
+            long memory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+            long totalMemory = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+
+            System.out.println("-------------------------------------------------------");
+            System.out.println("Total time: " + totalTime + "s");
+            System.out.println("Finished at: " + finishedAt);
+            System.out.println("Final Memory: " + memory + "M/ " + totalMemory + "M");
+            System.out.println("-------------------------------------------------------");
+        }));
+
+        try (
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            new Thread(() -> {
+                try {
+                    String fromServer;
+                    while (running && (fromServer = in.readLine()) != null) {
+                        if (fromServer.equals("SUBMITNAME")) {
+                            System.out.print("Enter your username: ");
+                        } else if (fromServer.startsWith("NAMEACCEPTED")) {
+                            System.out.println("Welcome " + fromServer.substring(13));
+                        } else {
+                            System.out.println(fromServer);
+                        }
+                    }
+                } catch (IOException e) {
+                    if (running) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            while (running) {
+                String userMessage = stdIn.readLine();
+                if (userMessage != null) {
+                    out.println(userMessage);
+                    if ("bye".equalsIgnoreCase(userMessage.trim())) {
+                        System.out.println("Disconnecting...");
+                        running = false;
+                    }
+                }
+            }
+
+            // Allow the reader thread to finish its execution
+            Thread.sleep(1000);
+        } catch (IOException ex) {
+            System.err.println("IOException: " + ex.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,141 @@
+/*
+Shijun Jiang
+CSCI 4311 Socket Programming (Assignment 1)
+Spring 2024 
+Server
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+    private static final int PORT = 8989;
+    private static Set<String> userNames = new HashSet<>();
+    private static Set<PrintWriter> writers = new HashSet<>();
+    private static Map<String, ZonedDateTime> userConnectionTimes = new ConcurrentHashMap<>();
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Server is Running...");
+        ServerSocket listener = new ServerSocket(PORT);
+
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private PrintWriter out;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                
+                
+                LocalTime timeNow = LocalTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss ");
+                String timeFormatted = timeNow.format(formatter);
+                
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null || name.isEmpty()) {
+                        return;
+                    }
+                    
+                    synchronized (userNames) {
+                        if (!userNames.contains(name)) {
+                            userNames.add(name);
+                            userConnectionTimes.put(name, ZonedDateTime.now());
+                            break;                         
+                        }
+                    }
+                }
+                out.println(timeFormatted + "Welcome " + name); 
+                for (PrintWriter writer : writers) {
+                    writer.println(" Server: Welcome " + name);
+                }
+                writers.add(out);                
+                
+                    while (true) {
+                        String message = in.readLine();
+                            if (message == null) {
+                                break; // This will handle the case of client disconnection
+                            }
+                            timeNow = LocalTime.now();
+                            timeFormatted = timeNow.format(formatter);             
+                            if (message.equalsIgnoreCase("Bye")) {
+                                synchronized (writers) {
+                                    for (PrintWriter writer : writers) {
+                                        writer.println(timeFormatted + "Server: Goodbye " + name);
+                                    }
+                                }
+                                return; 
+                            } 
+                            if (message.equalsIgnoreCase("AllUsers")) {
+                                StringBuilder userList = new StringBuilder();
+                                userList.append("List of the users connected at ")
+                                        .append(ZonedDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss zzz")))
+                                        .append("\n");
+                                
+                                int count = 1;
+                                synchronized (userNames) {
+                                    for (String userName : userNames) {
+                                        ZonedDateTime connectionTime = userConnectionTimes.get(userName);
+                                        userList.append(count++).append(") ").append(userName)
+                                                .append(" since ").append(connectionTime.format(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy")))
+                                                .append("\n");
+                                    }
+                                }
+                                out.println(userList.toString());
+                            } else {
+                                synchronized (writers) {
+                                    for (PrintWriter writer : writers) {
+                                        String broadcastMessage = timeFormatted + name + ": " + message;
+                                        writer.println(broadcastMessage);
+                                    }
+                                    System.out.println(timeFormatted + name + ": " + message); 
+                                }
+                            }                   
+                    }
+
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                
+                LocalTime timeNow = LocalTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss ");
+                String timeFormatted = timeNow.format(formatter);
+                
+                if (name != null) {
+                    userNames.remove(name);
+                    System.out.println(timeFormatted + name + " disconnected with a Bye message"); 
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                        socket.close();
+                    } catch (IOException e) {
+                        System.out.println("Exception when closing the socket: " + e.getMessage());
+                    }            
+            }
+        }
+    }
+}
